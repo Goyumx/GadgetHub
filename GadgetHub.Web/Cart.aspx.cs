12ecdbdf1af42341f5413e51cf31920d9e50c532
Goyumx <goyum.doingcode@gmail.com>
2025-07-24 22:50:48 +0530

    chore: Web form initialized

---
+++ GadgetHub.Web/Cart.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GadgetHub.Web.Models;
+
+namespace GadgetHub.Web
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadCart();
+            }
+        }
+
+        private void LoadCart()
+        {
+            var cart = Session["Cart"] as List<CartItem> ?? new List<CartItem>();
+            gvCart.DataSource = cart;
+            gvCart.DataBind();
+
+            decimal total = cart.Sum(item => item.Price * item.Quantity);
+            lblTotal.Text = $"Total: {total:C}";
+        }
+
+        protected void gvCart_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "RemoveItem")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                var cart = Session["Cart"] as List<CartItem>;
+                if (cart != null && cart.Count > index)
+                {
+                    cart.RemoveAt(index);
+                    Session["Cart"] = cart;
+                    LoadCart();
+                }
+            }
+        }
+    }
+}
---
+++ GadgetHub.Web/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using GadgetHub.Web.Models;
+using Newtonsoft.Json;
+
+namespace GadgetHub.Web
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected async void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                await LoadProductsAsync();
+            }
+        }
+
+        private async Task LoadProductsAsync()
+        {
+            string apiUrl = "https://localhost:7165/api/products"; // Replace with actual port
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri(apiUrl);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var response = await client.GetAsync("");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        var products = JsonConvert.DeserializeObject<List<ProductDto>>(json);
+                        rptProducts.DataSource = products;
+                        rptProducts.DataBind();
+                    }
+                    else
+                    {
+                        // Handle error
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log or display error
+                }
+            }
+        }
+    }
+}
