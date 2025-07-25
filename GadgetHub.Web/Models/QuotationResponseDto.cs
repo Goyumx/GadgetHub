c2b6c62e995b6fefcd8f5c52fe2bd3075083a435
Goyumx <goyum.doingcode@gmail.com>
2025-07-25 15:11:40 +0530

    feat: quotation section

---
+++ GadgetHub.Web/Models/QuotationResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GadgetHub.Web.Models
+{
+    public class QuotationResponseDto
+    {
+        public string ProductName { get; set; }
+        public string DistributorName { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public int AvailableQuantity { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
+    }
+}
---
+++ GadgetHub.Web/Quotation.aspx.cs
@@ -0,0 +1,77 @@
+﻿using GadgetHub.Web.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetHub.Web
+{
+    public partial class Quotation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected async void btnRequest_Click(object sender, EventArgs e)
+        {
+            var cart = Session["Cart"] as List<CartItem>;
+            if (cart == null || cart.Count == 0)
+            {
+                lblMessage.Text = "Your cart is empty.";
+                return;
+            }
+
+            var apiUrl = "https://localhost:7165/api/quotations";
+            var allQuotations = new List<QuotationResponseDto>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    foreach (var item in cart)
+                    {
+                        var singleRequest = new
+                        {
+                            ProductId = item.ProductId,
+                            Quantity = item.Quantity
+                        };
+
+                        var json = JsonConvert.SerializeObject(singleRequest);
+                        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                        var response = await client.PostAsync(apiUrl, content);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var result = await response.Content.ReadAsStringAsync();
+                            var quotations = JsonConvert.DeserializeObject<List<QuotationDto>>(result);
+
+                            // Map to QuotationResponseDto
+                            foreach (var quote in quotations)
+                            {
+                                allQuotations.Add(new QuotationResponseDto
+                                {
+                                    ProductName = quote.Product.Name,
+                                    DistributorName = quote.Distributor.Name,
+                                    PricePerUnit = quote.PricePerUnit,
+                                    AvailableQuantity = quote.AvailableQuantity,
+                                    EstimatedDeliveryDate = quote.EstimatedDeliveryDate
+                                });
+                            }
+                        }
+                    }
+
+                    gvQuotations.DataSource = allQuotations;
+                    gvQuotations.DataBind();
+                    lblMessage.Text = "Quotations retrieved successfully ✅";
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
+    }
+}
